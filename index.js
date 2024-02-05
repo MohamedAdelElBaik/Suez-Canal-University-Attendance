fff1668630d2f88da00a64bc4663b521a4ee3520
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-05 21:33:44 +0200

    LOGIC : redestruction for functions and parameters

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let data;
-let selectedSubId;
+let selectedSubId = 1;
+let selectedLecId = 1;
 
 $('document').ready(function () {
   fetch('./data/data.json')
@@ -7,48 +8,22 @@ $('document').ready(function () {
     .then((Data) => {
       // Your logic using the loaded JSON data
       data = Data;
-      addSubjectsNameToList(Data);
-      addLecturesDataToLectures(Data[0]);
-      addLectureDataToAttendance(Data[0].subLectures[0]);
-      selectedSubId = 1;
+
+      addSubjectsNameToList();
+
+      const lecturesData = getSelectedLecturesData();
+      addLecturesDataToLectures(lecturesData);
+
+      const lecData = getSelectedLecData();
+      addLectureDataToAttendance(lecData);
     })
     .catch((error) => {
       console.error('Error loading JSON:', error);
     });
 
-  function addSubjectsNameToList(data) {
-    // add subjects name to the list (add active for first one)
-    data.map((sub, index) =>
-      $('.list').append(
-        `<li data-id="${sub.id}" class="list--item ${
-          index === 0 && 'active'
-        }">${sub.subName}</li>`
-      )
-    );
-  }
-
-  function addLecturesDataToLectures(data) {
-    $('.lectures--card').remove();
-    data.subLectures.map((lec) =>
-      $('.lectures').append(
-        `<div data-id="${lec.id}" class="lectures--card">
-          <h2>المحاضرة</h2>
-          <span class="number">0${lec.id}</span>
-          <div><span class="date">${lec.date}</span></div>
-        </div>`
-      )
-    );
-  }
-
-  function addLectureDataToAttendance(data) {
-    $('#lec-title').text(data.title);
-    $('#lec-description').text(data.description);
-    $('#lec-date').text(data.date);
-  }
-
-  // control of click on subject list
-  // add active class
+  // control of click on subject list (add active class)
   $(document).on('click', '.list--item', function () {
+    selectedLecId = 1;
     // remove active class from all list items
     $('.list--item').removeClass('active');
 
@@ -61,11 +36,11 @@ $('document').ready(function () {
     selectedSubId = id;
 
     // update lectures data
-    const lecturesData = data.find((sub) => sub.id == id);
+    const lecturesData = getSelectedLecturesData();
     addLecturesDataToLectures(lecturesData);
 
     // update lecture data
-    const lecData = data.find((sub) => sub.id == selectedSubId).subLectures[0];
+    const lecData = getSelectedLecData();
     addLectureDataToAttendance(lecData);
 
     // Encode the clicked text to ensure it is URL-safe
@@ -79,12 +54,51 @@ $('document').ready(function () {
     // history.pushState(null, null, newUrl);
   });
 
+  // select lecture
   $(document).on('click', '.lectures--card', function () {
-    const selectedLecId = $(this).data().id;
+    selectedLecId = $(this).data().id;
 
-    const lecData = data
-      .find((sub) => sub.id == selectedSubId)
-      .subLectures.find((lec) => lec.id == selectedLecId);
+    const lecData = getSelectedLecData();
     addLectureDataToAttendance(lecData);
   });
 });
+
+function getSelectedLecturesData() {
+  return data.find((sub) => sub.id == selectedSubId);
+}
+
+function getSelectedLecData() {
+  return data
+    .find((sub) => sub.id == selectedSubId)
+    .subLectures.find((lec) => lec.id == selectedLecId);
+}
+
+function addSubjectsNameToList() {
+  // add subjects name to the list (add active for first one)
+  data.map((sub, index) =>
+    $('.list').append(
+      `<li data-id="${sub.id}" class="list--item ${index === 0 && 'active'}">${
+        sub.subName
+      }</li>`
+    )
+  );
+}
+
+function addLecturesDataToLectures(data) {
+  $('.lectures--card').remove();
+  data.subLectures.map((lec) =>
+    $('.lectures').append(
+      `<div data-id="${lec.id}" class="lectures--card">
+        <h2>المحاضرة</h2>
+        <span class="number">0${lec.id}</span>
+        <div><span class="date">${lec.date}</span></div>
+      </div>`
+    )
+  );
+}
+
+function addLectureDataToAttendance(data) {
+  $('#lec-title').text(data.title);
+  $('#lec-description').text(data.description);
+  $('#lec-date').text(data.date);
+}
