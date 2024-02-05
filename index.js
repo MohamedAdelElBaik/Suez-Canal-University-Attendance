8938aef3bb61b9477bda46250ef736954c020119
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-05 17:29:58 +0200

    LOGIC : fetch data and add subjects name to the list

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
+let data;
 $('document').ready(function () {
-  $('.list--item').click(function () {
+  fetch('./data/data.json')
+    .then((response) => response.json())
+    .then((dataVal) => {
+      // Your logic using the loaded JSON data
+      data = dataVal;
+      addSubjectsNameToList(data);
+    })
+    .catch((error) => {
+      console.error('Error loading JSON:', error);
+    });
+
+  function addSubjectsNameToList(data) {
+    // take subjects name from data.json file
+    data.map((sub, index) =>
+      $('.list').append(
+        `<li class="list--item ${index === 0 && 'active'}">${sub.subName}</li>`
+      )
+    );
+  }
+
+  // control of click on subject list
+  // add active class
+  $(document).on('click', '.list--item', function () {
     // remove active class from all list items
     $('.list--item').removeClass('active');
 
     // add class (active) to clicked item
     $(this).addClass('active');
+    $('#subject-title').text($(this).text());
+
+    // Encode the clicked text to ensure it is URL-safe
+    // var encodedText = encodeURIComponent($(this).text());
+
+    // Construct the new URL with the query parameter
+    // var newUrl =
+    // window.location.href.split('?')[0] + '?subject-name=' + encodedText;
+
+    // Push the new state to the browser's history
+    // history.pushState(null, null, newUrl);
   });
 });
