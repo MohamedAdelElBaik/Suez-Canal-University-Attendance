26a53df250025a4e108fb38056d019588c273155
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-06 02:18:35 +0200

    CLEAN : functions and add comments

--- index.js
+++ index.js
@@ -8,16 +8,10 @@ $('document').ready(function () {
   fetch('./data/data.json')
     .then((response) => response.json())
     .then((Data) => {
-      // Your logic using the loaded JSON data
       data = Data;
-
       addSubjectsNameToList();
-
-      const lecturesData = getSelectedLecturesData();
-      addLecturesDataToLectures(lecturesData);
-
-      const lecData = getSelectedLecData();
-      updateAttendanceSection(lecData);
+      addLecturesDataToLectures();
+      updateAttendanceSection();
     })
     .catch((error) => {
       console.error('Error loading JSON:', error);
@@ -25,35 +19,17 @@ $('document').ready(function () {
 
   // control of click on subject list (add active class)
   $(document).on('click', '.list--item', function () {
-    selectedLecId = 1;
+    selectedLecId = 1; // reset selected lec number whenever click in lec list
 
-    // clear active class for all list and add for selected
     clearActive('.list--item');
     addActive(this);
 
+    // add selected subject title in subject title
     $('#subject-title').text($(this).text());
 
-    // get data of subject that have this id
-    const id = $(this).data().id;
-    selectedSubId = id;
-
-    // update lectures data
-    const lecturesData = getSelectedLecturesData();
-    addLecturesDataToLectures(lecturesData);
-
-    // update lecture data
-    const lecData = getSelectedLecData();
-    updateAttendanceSection(lecData);
-
-    // Encode the clicked text to ensure it is URL-safe
-    // var encodedText = encodeURIComponent($(this).text());
-
-    // Construct the new URL with the query parameter
-    // var newUrl =
-    // window.location.href.split('?')[0] + '?subject-name=' + encodedText;
-
-    // Push the new state to the browser's history
-    // history.pushState(null, null, newUrl);
+    selectedSubId = $(this).data().id; // get id for selected subject
+    addLecturesDataToLectures(); // update lectures data
+    updateAttendanceSection();
   });
 
   // select lecture
@@ -61,10 +37,8 @@ $('document').ready(function () {
     clearActive('.lectures--card');
     addActive(this);
 
-    selectedLecId = $(this).data().id;
-
-    const lecData = getSelectedLecData();
-    updateAttendanceSection(lecData);
+    selectedLecId = $(this).data().id; // get id for selected lec
+    updateAttendanceSection();
   });
 
   // go to next page in table
@@ -73,7 +47,7 @@ $('document').ready(function () {
 
     // if there is data in next page go forward
     if (attendanceArr.length > tableItemsStartNum + tableItemsNum) {
-      tableItemsStartNum = tableItemsStartNum + tableItemsNum;
+      tableItemsStartNum += tableItemsNum;
       addAttendanceDataToTable(attendanceArrSlice());
     }
   });
@@ -82,7 +56,7 @@ $('document').ready(function () {
   $('#table-back').click(function () {
     // if there is data in previous page go back
     if (tableItemsStartNum > 0) {
-      tableItemsStartNum = tableItemsStartNum - tableItemsNum;
+      tableItemsStartNum -= tableItemsNum;
       addAttendanceDataToTable(attendanceArrSlice());
     }
   });
@@ -109,9 +83,11 @@ function addSubjectsNameToList() {
   );
 }
 
-function addLecturesDataToLectures(data) {
+function addLecturesDataToLectures() {
+  const lecturesData = getSelectedLecturesData();
+
   $('.lectures--card').remove();
-  data.subLectures.map((lec, index) =>
+  lecturesData.subLectures.map((lec, index) =>
     $('.lectures').append(
       `<div data-id="${lec.id}" class="lectures--card ${
         index === 0 ? 'active' : ''
@@ -125,10 +101,12 @@ function addLecturesDataToLectures(data) {
 }
 
 // let test;
-function updateAttendanceSection(data) {
+function updateAttendanceSection() {
+  const lecData = getSelectedLecData();
+
   tableItemsStartNum = 0; // reset page table number
-  addLectureDataToAttendance(data);
-  addAttendanceDataToTable(data.attendance.slice(0, tableItemsNum));
+  addLectureDataToAttendance(lecData);
+  addAttendanceDataToTable(lecData.attendance.slice(0, tableItemsNum));
 }
 
 function addLectureDataToAttendance(data) {
